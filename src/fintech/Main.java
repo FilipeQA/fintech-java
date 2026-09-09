7b1dee498a18fd81b0f251d404e828bbaacdd158
FilipeQA <filipeqamado@gmail.com>
2026-09-08 23:05:30 -0300

    feat: add Account class

--- src/fintech/Main.java
+++ src/fintech/Main.java
@@ -10,6 +10,14 @@ public class Main {
                 "filipe@email.com"
         );
 
+        Account account = new Account(1001, client);
+
+        account.deposit(1000);
+        account.withdraw(250);
+
+        System.out.println("Account: " + account.getAccountNumber());
+        System.out.println("Balance: R$ " + account.getBalance());
+
         System.out.println("Name: " + client.getName());
         System.out.println("CPF: " + client.getCpf());
         System.out.println("E-mail: " + client.getEmail());
